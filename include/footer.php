4c51d5935b9425f8d9c9df12e9a2ec3e1fc607c2
Marine Romeu <mromeu@stagiaire-humanbooster.com>
2021-10-11 16:10:52 +0200

    modif vecteurs footer

--- include/footer.php
+++ include/footer.php
@@ -30,11 +30,19 @@
     <div id="contact" class="col-md-2 my-2">
         <p>Une question ?</p>
         <a href="#">
-            <i class="bi bi-arrow-right"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+            </svg>
             Notre FAQ
         </a><br>
         <a href="#">
-            <i class="bi bi-arrow-right"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+            </svg>
             Contactez-nous
         </a>
     </div>
