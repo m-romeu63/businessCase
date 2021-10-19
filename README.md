42ec759c365659f061a5c184be52e6fbdfa830de
Marine Romeu <mromeu@stagiaire-humanbooster.com>
2021-10-19 15:35:34 +0200

    début du passage du procédural en orienté objet

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 businessCase
 
+An e-commerce website for a fictionnal pet shop (part of my development formation) developped with Bootstrap and Symfony.
+A e-commerce dashboard made with Angular will be linked to the main website.
--- models/Category.php
+++ models/Category.php
@@ -1,4 +1,88 @@
 <?php 
+    class Category extends Database {
+        private $id,
+                $name,
+                $parentId;
+
+        public function __construct() {
+        }
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId() {
+            return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id) {
+            $this->id = $id;
+            return $this;
+        }
+
+        /**
+         * Get the value of name
+         */ 
+        public function getName() {
+            return $this->name;
+        }
+
+        /**
+         * Set the value of name
+         *
+         * @return  self
+         */ 
+        public function setName($name){
+            $this->name = $name;
+            return $this;
+        }
+
+        /**
+         * Get the value of parentId
+          */ 
+        public function getParentId() {
+            return $this->parentId;
+        }
+
+        /**
+         * Set the value of parentId
+         *
+         * @return  self
+         */ 
+        public function setParentId($parentId) {
+            $this->parentId = $parentId;
+            return $this;
+        }
+
+        /**
+         * Method to get the category list from database
+         *
+         * @param Database $dbc
+         * @return void
+         */
+        public function getCategoryList($dbc) {
+            $query = 'SELECT * FROM products ORDER BY name';
+            $categories = $dbc->multipleSelect($query, PDO::FETCH_CLASS, __CLASS__);
+            return $categories;
+        }
+
+        /**
+         * Method to get one category from the database.
+         *
+         * @param Database $dbc
+         * @param int $id
+         * @return void
+         */
+        public function getCategory($dbc, $id) {
+            $query = 'SELECT * FROM products WHERE id = :id';
+            $category = $dbc->query($query, array('id' => $id));
+            return $category;
+        }
+    }
 $aCategories = array(
     array('id'=> 1,'categoryName'=>'Chien', 'icon'=>'fluent:animal-dog-20-filled'),
     array('id'=> 2,'categoryName'=>'Chat', 'icon'=>'fluent:animal-cat-20-filled'),
